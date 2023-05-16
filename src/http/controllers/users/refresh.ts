74ecb8677adbc8f834ffbd93eaab26378798ac89
wedersonf <wedymf@gmail.com>
2023-05-16 05:40:35 -0300

    feat: RBAC

--- src/http/controllers/users/refresh.ts
+++ src/http/controllers/users/refresh.ts
@@ -3,8 +3,12 @@ import { FastifyReply, FastifyRequest } from 'fastify'
 export async function refresh(request: FastifyRequest, reply: FastifyReply) {
   await request.jwtVerify({ onlyCookie: true })
 
+  const { role } = request.user
+
   const token = await reply.jwtSign(
-    {},
+    {
+      role,
+    },
     {
       sign: {
         sub: request.user.sub,
@@ -14,7 +18,9 @@ export async function refresh(request: FastifyRequest, reply: FastifyReply) {
   )
 
   const refreshToken = await reply.jwtSign(
-    {},
+    {
+      role,
+    },
     {
       sign: {
         sub: request.user.sub,
